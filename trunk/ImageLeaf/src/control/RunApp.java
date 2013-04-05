e45e297ba316c9628642f920bb48c9b938485ecd
andersonbrilhador@gmail.com <andersonbrilhador@gmail.com@56cfe0e7-b3da-eb93-09e3-f5431c5402ac>
2013-04-05 19:44:44 +0000

    ChainCode implemetaçao.

--- trunk/ImageLeaf/src/control/RunApp.java
+++ trunk/ImageLeaf/src/control/RunApp.java
@@ -18,7 +18,7 @@ public class RunApp {
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new ManageDirectory().criarDiretorio(new ManageDirectory().getDiretorioPadrao());
-                new ControlPrincipal();
+                new ControlMenu();
             }
         }); 
     }   
---
+++ trunk/ImageLeaf/src/model/ChainCode.java
@@ -0,0 +1,115 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+import java.awt.Dimension;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import model.Limiar;
+
+/**
+ *
+ * @author anderson
+ */
+public class ChainCode {
+
+    //variaveis globais    
+    private boolean[][] imageBorder = null;
+    private int width = 0;
+    private int heigth = 0;
+    private int startx = 0;
+    private int starty = 0;
+
+    //construtor
+    public ChainCode(boolean[][] imageBorder) {
+        this.imageBorder = imageBorder;
+        this.width = imageBorder.length;
+        this.heigth= imageBorder[0].length;
+        getInicialPixel();
+    }
+
+    private void getInicialPixel() {
+        //pega o pixel inicial
+        for (int i = width-1; i >= 0; i--) {
+            for (int j = heigth-1; j >= 0; j--) {
+                if (imageBorder[i][j]) {
+                    startx = i;
+                    starty = j;
+                    break;
+                }
+            }
+        }
+    }
+    
+    //pega o valor da direçao
+    private int getNextDirection(int x, int y){
+        int direction = 0;
+        while(direction < 8){
+            Dimension d = getNextCoordinates(x, y, direction);
+            if(d != null){
+                return direction;
+            }
+            direction++;
+        }
+        return -1;
+    }
+    
+    /*
+     * 321
+     * 4 0
+     * 567
+     */
+    private Dimension getNextCoordinates(int x, int y, int direction){
+        switch(direction){
+            case 0:
+                return ((inBounds(x+1, y)) && (isObjectLeaf(x+1, y)))? new Dimension(x+1, y) : null;
+            case 1:
+                return ((inBounds(x+1,y-1)) && (isObjectLeaf(x+1,y-1)))? new Dimension(x+1,y-1) : null;
+            case 2:
+                return ((inBounds(x, y-1)) && (isObjectLeaf(x, y-1)))? new Dimension(x, y-1) : null;
+            case 3:
+                return ((inBounds(x-1, y-1)) && (isObjectLeaf(x-1,y-1)))? new Dimension(x-1,y-1) : null;
+            case 4:
+                return ((inBounds(x-1,y)) && (isObjectLeaf(x-1,y)))? new Dimension(x-1,y) : null;
+            case 5:
+                return ((inBounds(x-1,y+1)) && (isObjectLeaf(x-1,y+1)))? new Dimension(x-1,y+1): null;
+            case 6:
+                return ((inBounds(x,y+1)) && (isObjectLeaf(x,y+1)))? new Dimension(x,y+1): null;
+            case 7:
+                return ((inBounds(x+1,y+1)) && (isObjectLeaf(x+1,y+1)))? new Dimension(x+1,y+1): null;
+            default:
+                return null;
+        }
+    }
+    
+    private boolean inBounds(int x, int y){
+        return ((x >= 0 && x < width) && (y >= 0 && y < heigth));
+    }
+    
+    private boolean isObjectLeaf(int x, int y){
+        return imageBorder[x][y];
+    }
+
+    public ArrayList<Dimension> getChainCode() {
+        //gera chain code
+        ArrayList<Dimension> lista = new ArrayList<>();
+        //coordenada auxiliares
+        int x = startx;
+        int y = starty;
+        do{
+            Dimension d = getNextCoordinates(x, y, getNextDirection(x, y));
+            if(d != null){
+                lista.add(d);
+                x = d.width;
+                y = d.height;
+                System.out.println(x + "," + y);
+            }else{
+                break;
+            }
+        }while(startx != x || starty != y);
+        System.out.println("finalizado");
+        return lista;
+    }
+}
