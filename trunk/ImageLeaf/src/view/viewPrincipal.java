c85b0466920e5722bcfcbe0e59de6044563f6817
andersonbrilhador@gmail.com <andersonbrilhador@gmail.com@56cfe0e7-b3da-eb93-09e3-f5431c5402ac>
2013-04-09 19:29:02 +0000

    Low-Pass implementado.

--- trunk/ImageLeaf/src/view/viewPrincipal.java
+++ trunk/ImageLeaf/src/view/viewPrincipal.java
@@ -34,27 +34,72 @@ public class viewPrincipal extends javax.swing.JFrame {
         jToolBar1 = new javax.swing.JToolBar();
         jPanelPrincipal = new javax.swing.JDesktopPane();
         jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
+        mFile = new javax.swing.JMenu();
         menuOpen = new javax.swing.JMenuItem();
+        mEdit = new javax.swing.JMenu();
+        mFilters = new javax.swing.JMenu();
+        mFilterBlur = new javax.swing.JMenu();
+        mFilterBlurLow = new javax.swing.JMenuItem();
+        jMenuItem3 = new javax.swing.JMenuItem();
         jMenu2 = new javax.swing.JMenu();
-        jMenu3 = new javax.swing.JMenu();
+        jMenuItem4 = new javax.swing.JMenuItem();
+        jMenuItem5 = new javax.swing.JMenuItem();
+        jMenuItem6 = new javax.swing.JMenuItem();
+        mCurvature = new javax.swing.JMenu();
+        jMenuItem2 = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jToolBar1.setRollover(true);
 
-        jMenu1.setText("File");
+        mFile.setText("File");
 
         menuOpen.setText("Open");
-        jMenu1.add(menuOpen);
+        mFile.add(menuOpen);
 
-        jMenuBar1.add(jMenu1);
+        jMenuBar1.add(mFile);
 
-        jMenu2.setText("Edit");
-        jMenuBar1.add(jMenu2);
+        mEdit.setText("Edit");
+        jMenuBar1.add(mEdit);
 
-        jMenu3.setText("Curvature");
-        jMenuBar1.add(jMenu3);
+        mFilters.setText("Filters");
+
+        mFilterBlur.setText("Blur");
+
+        mFilterBlurLow.setText("Low-Pass ");
+        mFilterBlurLow.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                mFilterBlurLowActionPerformed(evt);
+            }
+        });
+        mFilterBlur.add(mFilterBlurLow);
+
+        jMenuItem3.setText("Median");
+        mFilterBlur.add(jMenuItem3);
+
+        mFilters.add(mFilterBlur);
+
+        jMenu2.setText("Edge - Detect");
+
+        jMenuItem4.setText("Sobel");
+        jMenu2.add(jMenuItem4);
+
+        jMenuItem5.setText("Prewitt");
+        jMenu2.add(jMenuItem5);
+
+        jMenuItem6.setText("Roberts");
+        jMenu2.add(jMenuItem6);
+
+        mFilters.add(jMenu2);
+
+        jMenuBar1.add(mFilters);
+
+        mCurvature.setText("Curvature");
+
+        jMenuItem2.setText("Chain Code");
+        mCurvature.add(jMenuItem2);
+
+        jMenuBar1.add(mCurvature);
 
         setJMenuBar(jMenuBar1);
 
@@ -76,13 +121,26 @@ public class viewPrincipal extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void mFilterBlurLowActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_mFilterBlurLowActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_mFilterBlurLowActionPerformed
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenu jMenu3;
     private javax.swing.JMenuBar jMenuBar1;
+    private javax.swing.JMenuItem jMenuItem2;
+    private javax.swing.JMenuItem jMenuItem3;
+    private javax.swing.JMenuItem jMenuItem4;
+    private javax.swing.JMenuItem jMenuItem5;
+    private javax.swing.JMenuItem jMenuItem6;
     private javax.swing.JDesktopPane jPanelPrincipal;
     private javax.swing.JToolBar jToolBar1;
+    private javax.swing.JMenu mCurvature;
+    private javax.swing.JMenu mEdit;
+    private javax.swing.JMenu mFile;
+    private javax.swing.JMenu mFilterBlur;
+    private javax.swing.JMenuItem mFilterBlurLow;
+    private javax.swing.JMenu mFilters;
     private javax.swing.JMenuItem menuOpen;
     // End of variables declaration//GEN-END:variables
     
@@ -103,4 +161,46 @@ public class viewPrincipal extends javax.swing.JFrame {
     public void setMenuOpen(javax.swing.JMenuItem menuOpen) {
         this.menuOpen = menuOpen;
     }
+
+    public JMenu getmCurvature() {
+        return mCurvature;
+    }
+
+    public void setmCurvature(JMenu mCurvature) {
+        this.mCurvature = mCurvature;
+    }
+
+    public JMenu getmEdit() {
+        return mEdit;
+    }
+
+    public void setmEdit(JMenu mEdit) {
+        this.mEdit = mEdit;
+    }
+
+    public JMenu getmFile() {
+        return mFile;
+    }
+
+    public void setmFile(JMenu mFile) {
+        this.mFile = mFile;
+    }
+
+    public JMenu getmFilters() {
+        return mFilters;
+    }
+
+    public void setmFilters(JMenu mFilters) {
+        this.mFilters = mFilters;
+    }
+
+    public JMenuItem getmFilterBlurLow() {
+        return mFilterBlurLow;
+    }
+
+    public void setmFilterBlurLow(JMenuItem mFilterBlurLow) {
+        this.mFilterBlurLow = mFilterBlurLow;
+    }
+
+    
 }
